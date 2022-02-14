285344ba1cf84e448c18b53ff60372e526b96ae5
NicholasYaw <yawfz-wm19@student.tarc.edu.my>
2022-02-14 13:54:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practical33_externalactivity/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.practical33_externalactivity
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnA: Button = findViewById(R.id.btnA)
+
+
+        btnA.setOnClickListener() {
+            val intentA: Intent = Intent(this, AboutActivity::class.java)
+
+            intentA.putExtra("StudentName", "Nicholas Yaw")
+
+            startActivity(intentA)
+        }
+
+        val btnB: Button = findViewById(R.id.btnB)
+
+
+        btnB.setOnClickListener() {
+            // val telNo = Uri.parse("tel:0169881380")
+            //val intentB: Intent = Intent(Intent.ACTION_DIAL, telNo)
+
+            val geo = Uri.parse("geo:3.140853, 101.693207")
+            val intentB: Intent = Intent(Intent.ACTION_VIEW, geo)
+            startActivity(intentB)
+        }
+    }
+}
